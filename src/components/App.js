abf76e31d5496f8e122f54d2b28a24e82b072c9a
ktoroshchcin <k.toroshchin@gmail.com>
2019-02-15 15:05:46 -0500

    Testing

--- src/components/App.js
+++ src/components/App.js
@@ -18,9 +18,14 @@ class App extends React.Component {
             }
         });
        this.setState({
-           videos: response.data.items
+           videos: response.data.items,
+           selectedVideo: response.data.items[0]
        })
     };
+
+    componentDidMount() {
+        this.onTermSubmit("buildings")
+    }
 
     onVideoSelect = (video) => {
         this.setState({selectedVideo: video})
